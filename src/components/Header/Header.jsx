958edbd1d289d0ae7f5022b7d54a13b1815fcff7
JafarLihammed <124575685+JafarLihammed@users.noreply.github.com>
2023-04-26 14:59:23 +0100

    Header component done and responsive

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,25 +1,44 @@
-import React from 'react'
-import './Header.css'
-import {FaGithub} from 'react-icons/fa'
-import {TbBrightnessUp} from 'react-icons/tb'
+import React, { useState } from "react";
+import "./Header.css";
+import { FaGithub, FaBars } from "react-icons/fa";
+import { RxCross1 } from "react-icons/rx";
+import { TbBrightnessUp } from "react-icons/tb";
 
 const Header = () => {
+  const [isNavExpanded, setIsNavExpanded] = useState(false);
   return (
-    <section className='header'>
-        <div>jafarscript</div>
-        <nav>
-            <ul>
-                <li><a href="">Home</a></li>
-                <li><a href="">About</a></li>
-                <li><a href="">Work</a></li>
-            </ul>
-            <a href="https://github.com/Jafarscript"><FaGithub  style={{fontSize: '1.5rem'}}/></a>
-            <TbBrightnessUp style={{cursor: 'pointer', fontSize: '1.5rem'}} />
-            <button>{'Contact me ->'}</button>
+    <section className="header">
+      <div>jafarscript</div>
+      <div className="navBar">
+        <nav className={ isNavExpanded ? 'navLink expanded' : 'navLink'} onClick={() => {setIsNavExpanded(!isNavExpanded)}}>
+          <ul>
+            <li>
+              <a href="">Home</a>
+            </li>
+            <li>
+              <a href="">About</a>
+            </li>
+            <li>
+              <a href="">Work</a>
+            </li>
+          </ul>
         </nav>
-
+        <a href="https://github.com/Jafarscript">
+          <FaGithub style={{ fontSize: "1.2rem" }} />
+        </a>
+        <TbBrightnessUp style={{ cursor: "pointer", fontSize: "1.2rem" }} />
+        <button className="contactBtn">{"Contact me ->"}</button>
+        <button
+          className="navToggler"
+          onClick={() => {
+            setIsNavExpanded(!isNavExpanded);
+          }}
+        >
+          {isNavExpanded ? <RxCross1 /> : <FaBars />}
+        </button>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Hero.css'
+
+const Hero = () => {
+  return (
+    <section>
+        <h1>Hello I’m Jafar Li-Hammed </h1>
+    </section>
+  )
+}
+
+export default Hero
