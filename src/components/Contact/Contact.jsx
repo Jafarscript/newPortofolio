cc085644d0f760ddc30a682555d6ec14759b9609
JafarLihammed <124575685+JafarLihammed@users.noreply.github.com>
2023-04-27 20:01:09 +0100

    Website design completed it remain some configuration

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -4,7 +4,7 @@ import './Contact.css'
 const Contact = ({dark}) => {
   return (
     <div className='contact'>
-        <h1 className={dark ? 'dark' : 'light'}>Ready to start your next project?<br /><span className="Comap"><a href="https://wa.me/07069013967?text=I'm%20interested%20in%20creating%20my%20business%20website">Get it touch me!</a></span></h1>
+        <h1 className={dark ? 'dark' : 'light'}>Ready to start your next project?<br /><span className="Comap"><a href="https://wa.me/7069013967?text=I'm%20interested%20in%20creating%20my%20business%20website">Get it touch me!</a></span></h1>
     </div>
   )
 }
