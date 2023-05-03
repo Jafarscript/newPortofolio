14b4e98df5b5a9e1964ca6d1747a8a806d3db35d
JafarLihammed <124575685+JafarLihammed@users.noreply.github.com>
2023-05-03 11:39:55 +0100

    Treated some links of the project

--- src/components/Work/Work.jsx
+++ src/components/Work/Work.jsx
@@ -15,7 +15,7 @@ const Work = ({dark}) => {
             <h2 className="projectName">Rock,Paper&Scissors</h2>
             <h5 className={dark ? 'dateDark' : 'date'}>2022</h5>
           </div>
-          <div className="projectImage"></div>
+          <div className="projectImage" style={{backgroundImage: 'url(https://res.cloudinary.com/dvo8xhx6r/image/upload/v1683109792/desktop-step-1_lc68sp.jpg)'}}></div>
           <div className="projectStack">
           <div className="projectSkills">
               <span className={dark ? 'skillDark' : 'skillLight'}>HTML</span>
@@ -23,10 +23,10 @@ const Work = ({dark}) => {
               <span className={dark ? 'skillDark' : 'skillLight'}>React</span>
             </div>
             <div className="projectLink">
-              <a href="https://github.com/Jafarscript">
+              <a href="https://github.com/Jafarscript/React-Game" target="_blank" rel="noopener noreferrer">
                 <FaGithub />
               </a>
-              <a href="https://github.com/Jafarscript">
+              <a href="https://rps-game101.netlify.app" target="_blank" rel="noopener noreferrer" >
                 <FiExternalLink />
               </a>
             </div>
@@ -37,10 +37,10 @@ const Work = ({dark}) => {
         </div>
         <div className={dark ? 'eachProject eachDark' : 'eachProject eachLight'}>
           <div className="projectHead">
-            <h2 className="projectName">Rock,Paper&Scissors</h2>
+            <h2 className="projectName">PowerGym</h2>
             <h5 className={dark ? 'dateDark' : 'date'}>2022</h5>
           </div>
-          <div className="projectImage"></div>
+          <div className="projectImage" style={{backgroundImage: 'url(https://res.cloudinary.com/dvo8xhx6r/image/upload/v1683109190/maxresdefault_ttbim3.jpg)'}}></div>
           <div className="projectStack">
             <div className="projectSkills">
               <span className={dark ? 'skillDark' : 'skillLight'}>HTML</span>
@@ -48,24 +48,24 @@ const Work = ({dark}) => {
               <span className={dark ? 'skillDark' : 'skillLight'}>React</span>
             </div>
             <div className="projectLink">
-              <a href="https://github.com/Jafarscript">
+              <a href="https://github.com/Jafarscript/gymClub"  target="_blank" rel="noopener noreferrer">
                 <FaGithub />
               </a>
-              <a href="https://github.com/Jafarscript">
+              <a href="https://powergym450.netlify.app" target="_blank" rel="noopener noreferrer">
                 <FiExternalLink />
               </a>
             </div>
           </div>
           <div className="projectDetials">
-          Rock,Paper and Scissors game build with react JS
+          In this club we will help build you ideal body for male and female
           </div>
         </div>
         <div className={dark ? 'eachProject eachDark' : 'eachProject eachLight'}>
           <div className="projectHead">
-            <h2 className="projectName">Rock,Paper&Scissors</h2>
+            <h2 className="projectName">AL-MANAT</h2>
             <h5 className={dark ? 'dateDark' : 'date'}>2022</h5>
           </div>
-          <div className="projectImage"></div>
+          <div className="projectImage"  style={{backgroundImage: 'url(https://res.cloudinary.com/dvo8xhx6r/image/upload/v1683110020/travel-agency-web-design_txi7j5.jpg)'}}></div>
           <div className="projectStack">
           <div className="projectSkills">
               <span className={dark ? 'skillDark' : 'skillLight'}>HTML</span>
@@ -73,41 +73,42 @@ const Work = ({dark}) => {
               <span className={dark ? 'skillDark' : 'skillLight'}>React</span>
             </div>
             <div className="projectLink">
-              <a href="https://github.com/Jafarscript">
+              <a href="https://github.com/Jafarscript" target="_blank" rel="noopener noreferrer">
                 <FaGithub />
               </a>
-              <a href="https://github.com/Jafarscript">
+              <a href="https://al-manat.netlify.app" target="_blank" rel="noopener noreferrer">
                 <FiExternalLink />
               </a>
             </div>
           </div>
           <div className="projectDetials">
-          Rock,Paper and Scissors game build with react JS
+          Travels and Tours agency, Umrah and Hajj agency website
           </div>
         </div>
         <div className={dark ? 'eachProject eachDark' : 'eachProject eachLight'}>
           <div className="projectHead">
-            <h2 className="projectName">Rock,Paper&Scissors</h2>
+            <h2 className="projectName">Short Video App</h2>
             <h5 className={dark ? 'dateDark' : 'date'}>2022</h5>
           </div>
-          <div className="projectImage"></div>
+          <div className="projectImage" style={{backgroundImage: 'url(https://res.cloudinary.com/dvo8xhx6r/image/upload/v1683110344/20201031052954_detail.036_atjb3v.jpg)'}}></div>
           <div className="projectStack">
           <div className="projectSkills">
-              <span className={dark ? 'skillDark' : 'skillLight'}>HTML</span>
-              <span className={dark ? 'skillDark' : 'skillLight'}>CSS</span>
-              <span className={dark ? 'skillDark' : 'skillLight'}>React</span>
+              <span className={dark ? 'skillDark' : 'skillLight'}>Express Js</span>
+              <span className={dark ? 'skillDark' : 'skillLight'}>Node Js</span>
+              <span className={dark ? 'skillDark' : 'skillLight'}>Mondo DB</span>
+              <span className={dark ? 'skillDark' : 'skillLight'}>React Js</span>
             </div>
             <div className="projectLink">
-              <a href="https://github.com/Jafarscript">
+              <a href="https://github.com/Jafarscript/short-mern-video" target="_blank" rel="noopener noreferrer">
                 <FaGithub />
               </a>
-              <a href="https://github.com/Jafarscript">
+              <a href="hhttps://short-video-app.netlify.app" target="_blank" rel="noopener noreferrer">
                 <FiExternalLink />
               </a>
             </div>
           </div>
           <div className="projectDetials">
-          Rock,Paper and Scissors game build with react JS
+          Short Video App created with MERN stack
           </div>
         </div>
       </main>
